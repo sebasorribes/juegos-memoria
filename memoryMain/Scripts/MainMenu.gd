fcf7657891ad27b12ad31801062104fc2527387d
sebasorribes <sebastiansorribes@gmail.com>
2025-12-22 15:30:51 -0300

    Update project title and rebuild Godot exports
    Changed the HTML page title from 'memoryMain' to 'memory' and updated Godot export files (index.pck, index.wasm, index.js) and project resources to reflect recent changes in the Godot project. This includes updated file sizes and rebuilt assets for deployment.

--- memoryMain/Scripts/MainMenu.gd
+++ memoryMain/Scripts/MainMenu.gd
@@ -1,8 +1,28 @@
 extends Node
 
+var creditsPanel : Control;
+var buttonsPanel : Control;
 
+func _ready() -> void:
+    creditsPanel = get_node("CreditsPanel");
+    buttonsPanel = get_node("Mainbuttons");
+    creditsPanel.hide();
+    buttonsPanel.show();
 
 func _on_match_mode_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Match/match_game.tscn");
-    
+    get_tree().change_scene_to_file("res://Scenes/Match/match_game.tscn");	
 
+func _on_paisajes_mode_pressed() -> void:
+    pass
+
+func _on_compras_pressed() -> void:
+    pass
+
+
+func _on_credits_button_pressed() -> void:
+    buttonsPanel.hide();
+    creditsPanel.show();
+
+func _on_exit_credits_pressed() -> void:
+    creditsPanel.hide();
+    buttonsPanel.show();
